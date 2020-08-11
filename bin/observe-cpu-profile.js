1df569566d04daf5908146ecb97c47dee3438112
RafaelGSS <rafael.nunu@hotmail.com>
2020-08-11 07:36:42 -0300

    doc: add unit of measurement in duration on profilers

--- bin/observe-cpu-profile.js
+++ bin/observe-cpu-profile.js
@@ -4,7 +4,7 @@ const { run } = require('../lib/commands/cpu-profile');
 const { program, runCommand } = require('./common');
 
 program
-  .option('-d, --duration <duration>', 'how long the profiler should run', parseInt)
+  .option('-d, --duration <duration>', 'how long the profiler should run (in seconds)', parseInt)
   .option('-s, --sampling-interval <interval>', 'sampling rate (in microseconds)', parseInt)
 
 runCommand(run, program);
--- bin/observe-heap-profile.js
+++ bin/observe-heap-profile.js
@@ -5,6 +5,6 @@ const { program, runCommand } = require('./common');
 
 program
   .option('-s, --sampling-interval <interval>', 'sampling rate (in bytes)', parseInt)
-  .option('-d, --duration <duration>', 'how long the profiler should run', parseInt)
+  .option('-d, --duration <duration>', 'how long the profiler should run (in seconds)', parseInt)
 
 runCommand(run, program);
