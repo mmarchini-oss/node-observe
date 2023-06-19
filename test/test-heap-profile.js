65c361c7dab2d5e95002975f9b70d55c2aa90b69
Mary Marchini <oss@mmarchini.me>
2023-06-19 13:10:38 -0700

    feat: add --close flag
    Add --close flag to close inspector protocol host on the inspected
    process. This might kill existing connections from other tools, or could
    block the inspected process' event loop, so use at your own risk.

--- test/test-heap-profile.js
+++ test/test-heap-profile.js
@@ -3,29 +3,19 @@
 const test = require('tape');
 
 const { run } = require('../lib/commands/heap-profile');
-const { startFixtureProcess } = require('./common');
+const { startFixtureProcess, runCommandWithClient } = require('./common');
 
-class FakeStream {
-  constructor() {
-    this.data = '';
-  }
-
-  write(chunk) {
-    this.data += chunk;
-  }
-}
 
 test('take heap profile', (t) => {
   const f = startFixtureProcess(t, true);
   f.on('inspectorReady', async ({ port }) => {
     t.ok(port > 0);
 
-    const stream = new FakeStream();
     // Mock setTimeout
     const oldSetTimeout = setTimeout;
     // eslint-disable-next-line no-global-assign
     setTimeout = (fn) => oldSetTimeout(fn, 1 * 1000);
-    await run('localhost', port, stream);
+    const stream = await runCommandWithClient(run, port);
     // eslint-disable-next-line no-global-assign
     setTimeout = oldSetTimeout;
     t.notEqual(stream.data.length, 0);
@@ -43,8 +33,7 @@ test('take heap profile with duration', (t) => {
   f.on('inspectorReady', async ({ port }) => {
     t.ok(port > 0);
 
-    const stream = new FakeStream();
-    await run('localhost', port, stream, { duration: 1 });
+    const stream = await runCommandWithClient(run, port, { duration: 1 });
 
     t.notEqual(stream.data.length, 0);
     const { head } = JSON.parse(stream.data);
@@ -62,11 +51,10 @@ test('take heap profile with samplingInterval', (t) => {
   f.on('inspectorReady', async ({ port }) => {
     t.ok(port > 0);
 
-    const stream1 = new FakeStream();
-    await run('localhost', port, stream1, { duration: 1, });
+    const stream1 = await runCommandWithClient(run, port, { duration: 1, });
 
-    const stream2 = new FakeStream();
-    await run('localhost', port, stream2, { duration: 1, samplingInterval : 1024 });
+
+    const stream2 = await runCommandWithClient(run, port, { duration: 1, samplingInterval : 1024 });
 
     // The second run samples 500 times more, so it's reasonable to assume more
     // frames will be captured, thus the profile should be bigger, even if it
